c2c944b6345b0cc35f644ef8a8e986dc237ee5e8
jeffreywsprague-dotcom <jeffreywsprague@gmail.com>
2026-09-10 21:31:11 -0400

    Update ping.js

--- ping.js
+++ ping.js
@@ -1,10 +1,1 @@
-exports.handler = async function () {
-  return {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ ok: true, time: Date.now() }),
-  };
-};
+
